8eba57cbc497350c21ef1c65b34db30556ca0693
Buh Duy <148170993+BuhDuy256@users.noreply.github.com>
2024-10-31 11:34:39 +0700

    Implement Button Class

---
+++ Mario Game/Button.cpp
@@ -0,0 +1,79 @@
+#include "Button.hpp"
+
+Button::Button(sf::RenderWindow& window, const sf::Texture& defaultTexture, const sf::Texture& hoveredTexture, const sf::Texture& pressedTexture, const sf::Text text, const sf::Font font, void(*action)(), Object* parent)
+{
+	m_window = &window;
+	m_defaultTexture = defaultTexture;
+	m_hoveredTexture = hoveredTexture;
+	m_pressedTexture = pressedTexture;
+	m_text = text;
+	m_text.setColor(sf::Color::Cyan);
+	m_font = font;
+	m_action = action;
+	m_parent = parent;
+}
+
+Button::Button(sf::RenderWindow& window, const sf::Texture& texture, const sf::Text text, const sf::Font font, void(*action)(), Object* parent) : Button(window, texture, texture, texture, text, font, action, parent) {}
+
+Button::~Button() {}
+
+void Button::setDefaultTexture(const Texture& defaultTexture)
+{
+	m_defaultTexture = defaultTexture;
+}
+
+void Button::setHoveredTexture(const Texture& hoveredTexture)
+{
+	m_hoveredTexture = hoveredTexture;
+}
+
+void Button::setPressedTexture(const Texture& pressedTexture)
+{
+	m_pressedTexture = pressedTexture;
+}
+
+void Button::setFont(const sf::Font& font)
+{
+	m_font = font;
+}
+
+void Button::setText(const sf::Text& text)
+{
+	m_text = text;
+}
+
+void Button::onHovered()
+{
+	if (m_isHovered)
+	{
+		m_text.setColor(sf::Color::White);
+		m_background.setTexture(m_hoveredTexture);
+	}
+}
+
+void Button::onPressed()
+{
+	if (m_isPressed)
+	{
+		m_text.setColor(sf::Color::Cyan);
+		m_background.setTexture(m_pressedTexture);
+		m_background.getSprite().setScale(0.95f, 0.95f);
+	}
+}
+
+void Button::onClick()
+{
+	if (m_isPressed)
+	{
+		m_text.setColor(sf::Color::Cyan);
+		m_background.setTexture(m_defaultTexture);
+		m_background.getSprite().setScale(1.0f, 1.0f);
+		if (m_action)
+			m_action();
+	}
+}
+
+void Button::setAction(void(*func)())
+{
+	m_action = func;
+}
---
+++ Mario Game/Button.hpp
@@ -0,0 +1,48 @@
+﻿#pragma once
+
+#include "Common.hpp"
+#include "GUI.hpp"
+#include "SFML/Graphics.hpp"
+
+/*
+When button is not hovered (set Default):
+	- Color of text is a color != white
+	- Texture is m_notHoveredTexture
+When button is hovered (call onHovered()):
+	- Color of text is white
+	- Texture is m_hoveredTexture
+When button is pressed (call onPressed()): 
+	- Color of text is a color != white
+	- Texture is m_pressedTexture
+	- Scale background's sprite to 0.95 (Reduce very small to just for User know)
+When button is released (call onClicked()) == When button is not hovered (set Default)
+*/
+
+class Button : public GUI
+{
+private:
+	sf::Texture m_defaultTexture;
+	sf::Texture m_hoveredTexture;
+	sf::Texture m_pressedTexture;
+	sf::Text m_text;
+	sf::Font m_font;
+	void (*m_action) ();
+
+public:
+	Button(sf::RenderWindow& window,const sf::Texture& defaultTexture, const sf::Texture& hoveredTexture, const sf::Texture& pressedTexture, const sf::Text text, const sf::Font font, void (*action) () = nullptr, Object* parent = nullptr);
+	Button(sf::RenderWindow& window, const sf::Texture& texture, const sf::Text text, const sf::Font font, void (*action) () = nullptr, Object* parent = nullptr); // defaultTexture == hoveredTexture == pressedTexture
+	~Button();
+
+	void setDefaultTexture(const Texture& defaultTexture);
+	void setHoveredTexture(const Texture& hoveredTexture);
+	void setPressedTexture(const Texture& pressedTexture);
+
+	void setFont(const sf::Font& font);
+	void setText(const sf::Text& text);
+
+	void onHovered() override;
+	void onPressed() override;
+	void onClick() override;
+
+	void setAction(void (*func) ());
+};
