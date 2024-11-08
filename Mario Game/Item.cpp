062783cce8274149fba97353ed0624920985dc7b
GiaHuy <giahuytran4205@gmail.com>
2024-11-08 23:04:25 +0700

    Add class Item

---
+++ Mario Game/Item.cpp
@@ -0,0 +1,48 @@
+#include "Item.hpp"
+#include "GameManager.hpp"
+
+Item::Item(Object* parent) {
+
+}
+
+Item::Item(ItemType type, Object* parent) : m_type(type), m_isPlayAnim(true) {
+	m_parent = parent;
+	m_elapsedTime = 0;
+	m_index = 0;
+}
+
+Item::Item(const Item& item) {
+
+}
+
+Item::~Item() {
+
+}
+
+void Item::setAnim(vector<Frame>& anim) {
+	m_anim = anim;
+}
+
+void Item::addFrame() {
+
+}
+
+void Item::playAnim() {
+
+}
+
+void Item::stopAnim() {
+
+}
+
+void Item::update() {
+	if (m_isPlayAnim) {
+		if (m_elapsedTime <= 0) {
+			m_elapsedTime = m_anim[m_index].duration;
+			m_sprite.setTexture(*m_anim[m_index].frame);
+			m_index++;
+			m_index %= m_anim.size();
+		}
+		else m_elapsedTime -= deltaTime.asMilliseconds();
+	}
+}
---
+++ Mario Game/Item.hpp
@@ -0,0 +1,37 @@
+#pragma once
+#include "Block.hpp"
+#include "TileSet.hpp"
+#include <vector>
+using namespace std;
+
+enum ItemType {
+	OneUpMushroom,
+	Coin,
+	KoopaShell,
+	BuzyShell,
+	MagicMushroom,
+	FireFlower,
+	Starman
+};
+
+class Item : public Block {
+private:
+	ItemType m_type;
+	bool m_isPlayAnim;
+	float m_elapsedTime;
+	int m_index;			// current index of m_anim
+	vector<Frame> m_anim;
+
+public:
+	Item(Object* parent = nullptr);
+	Item(ItemType type, Object* parent = nullptr);
+	Item(const Item& item);
+	~Item();
+
+	void setAnim(vector<Frame>& anim);
+	void addFrame();
+	void playAnim();
+	void stopAnim();
+
+	void update() override;
+};
