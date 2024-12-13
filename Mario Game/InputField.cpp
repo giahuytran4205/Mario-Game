7c1fe4e5cb51c47bfc012dbbad3d6f3eb85d5cef
GiaHuy <giahuytran4205@gmail.com>
2024-12-13 16:54:45 +0700

    Update InputField

--- Mario Game/InputField.cpp
+++ Mario Game/InputField.cpp
@@ -2,145 +2,147 @@
 
 InputField* InputField::currentlySelectedField = nullptr;
 
-FRect InputField::getRectOfShape()
+void InputField::adjustTextPosCharSizeInBox()
 {
-    return { m_box.getPosition().x, m_box.getPosition().y, m_box.getSize().x,  m_box.getSize().y };
+	m_text.setString("Temp Text");
+
+	float boxHeight = m_box.getSize().y;
+	float boxWidth = m_box.getSize().x;
+
+	m_text.setCharacterSize(20);
+
+	while (true)
+	{
+		sf::FloatRect textBounds = m_text.getGlobalBounds();
+		if (textBounds.height > boxHeight || textBounds.width > boxWidth)
+		{
+			m_text.setCharacterSize(m_text.getCharacterSize() - 1);
+		}
+		else
+		{
+			break;
+		}
+	}
+
+	sf::FloatRect textBounds = m_text.getGlobalBounds();
+	float textX = m_box.getPosition().x + boxWidth / 20.0f;
+	float textY = m_box.getPosition().y + (boxHeight - textBounds.height) / 3.0f;
+	m_text.setPosition(textX, textY);
+	m_text.setString("");
+
+	m_transform.getRect() = { m_box.getPosition().x, m_box.getPosition().y, m_box.getSize().x,  m_box.getSize().y };
 }
 
-void InputField::adjustTextPosCharSizeInBox(sf::Text& text, const sf::RectangleShape& box)
+InputField::InputField(Object* parent)
 {
-    text.setString("Temp Text");
+	setParent(parent);
 
-    float boxHeight = box.getSize().y;
-    float boxWidth = box.getSize().x;
+	this->setRenderOrder(INPUTFIELD_DEFAULT_RENDER_ORDER);
 
-    text.setCharacterSize(20);
-
-    while (true)
-    {
-        sf::FloatRect textBounds = text.getGlobalBounds();
-        if (textBounds.height > boxHeight || textBounds.width > boxWidth)
-        {
-            text.setCharacterSize(text.getCharacterSize() - 1);
-        }
-        else
-        {
-            break;
-        }
-    }
-
-    sf::FloatRect textBounds = text.getGlobalBounds();
-    float textX = box.getPosition().x + boxWidth / 20.0f;
-    float textY = box.getPosition().y + (boxHeight - textBounds.height) / 3.0f;
-    text.setPosition(textX, textY);
-    text.setString("");
-
-    m_transform.getRect() = { m_box.getPosition().x, m_box.getPosition().y, m_box.getSize().x,  m_box.getSize().y };
+	this->configure(sf::Vector2f(0, 0), sf::Vector2f(0, 0), sf::Font());
 }
 
-InputField::InputField(Object* parent, sf::RenderWindow& window, const float& x, const float& y, const float& width, const float& height, sf::Font& font)
+InputField::InputField(const sf::Vector2f& position, const sf::Vector2f& size, const sf::Font& font, Object* parent)
 {
-    setParent(parent);
-    m_box.setSize({ width, height });
-    m_box.setPosition({ x, y });
-    m_box.setFillColor(sf::Color::White);
-    m_box.setOutlineThickness(1.5);
-    m_box.setOutlineColor(sf::Color::Black);
+	setParent(parent);
 
-    m_text.setFont(font);
-    adjustTextPosCharSizeInBox(m_text, m_box);
-    m_text.setFillColor(sf::Color::Black);
+	this->setRenderOrder(INPUTFIELD_DEFAULT_RENDER_ORDER);
 
-    m_cursor.setSize({ 2.f, 24.f });
-    m_cursor.setFillColor(sf::Color::Black);
-    m_cursorVisible = true;
-    m_cursorBlinkClock.restart();
+	this->configure(position, size, font);
 }
 
-void InputField::onClick()
+void InputField::configure(const sf::Vector2f& position, const sf::Vector2f& size, const sf::Font& font)
 {
-    currentlySelectedField = this;
+	m_box.setPosition(position);
+	m_box.setSize(size);
+	m_box.setFillColor(sf::Color::White);
+	m_box.setOutlineThickness(1.0f);
+	m_box.setOutlineColor(sf::Color::Black);
+
+	m_text.setFont(font);
+	this->adjustTextPosCharSizeInBox();
+	m_text.setFillColor(sf::Color::Black);
+
+	m_cursor.setSize(sf::Vector2f(2.f, m_text.getCharacterSize()));
+	m_cursor.setFillColor(sf::Color::Black);
+	m_cursorVisible = true;
+	m_cursorBlinkClock.restart();
 }
 
-void InputField::onKeyPressed(const sf::Event::TextEvent& textEvent)
+void InputField::setPosition(const sf::Vector2f& position)
 {
-    if (currentlySelectedField == this)
-    {
-        if (textEvent.unicode == 8)
-        {
-            if (!m_content.empty())
-            {
-                m_content.pop_back();
-                m_text.setString(m_content);
-            }
-        }
-        else if (textEvent.unicode >= 32 && textEvent.unicode < 127)
-        {
-            m_content += static_cast<char>(textEvent.unicode);
-            m_text.setString(m_content);
-        }
-    }
+	m_box.setPosition(position);
+	this->adjustTextPosCharSizeInBox();
+	m_transform.getRect() = { position.x, position.y, m_box.getSize().x, m_box.getSize().y };
 }
 
-void InputField::setPosition(float x, float y)
+void InputField::setSize(const sf::Vector2f& size)
 {
-    m_box.setPosition(x, y);
-    this->adjustTextPosCharSizeInBox(m_text, m_box);
-    FRect newRect = { x, y, m_box.getSize().x, m_box.getSize().y };
-    m_transform.getRect() = newRect;
-}
-
-void InputField::setSize(float width, float height)
-{
-    m_box.setSize({ width, height });
-    this->adjustTextPosCharSizeInBox(m_text, m_box);
-    FRect newRect = { m_box.getPosition().x, m_box.getPosition().y, m_box.getSize().x, m_box.getSize().y };
-    m_transform.getRect() = newRect;
-}
-
-sf::Vector2f InputField::getPosition()
-{
-    return m_box.getPosition();
-}
-
-sf::Vector2f InputField::getSize()
-{
-    return m_box.getSize();
+	m_box.setSize(size);
+	this->adjustTextPosCharSizeInBox();
+	m_transform.getRect() = { m_box.getPosition().x, m_box.getPosition().y, size.x, size.y };
 }
 
 std::string InputField::getContent()
 {
-    return m_text.getString();
+	return m_text.getString();
 }
 
 void InputField::clearContent()
 {
-    m_content.clear();
-    m_text.setString(m_content);
+	m_content.clear();
+	m_text.setString(m_content);
 }
 
-void InputField::draw(sf::RenderWindow& target)
+void InputField::onClick()
 {
-    target.draw(m_box);
-    m_text.setString(m_content);
-    target.draw(m_text);
+	currentlySelectedField = this;
+}
 
-    if (m_isSelected)
-    {
-        if (m_cursorBlinkClock.getElapsedTime().asSeconds() >= 0.5f)
-        {
-            m_cursorVisible = !m_cursorVisible;
-            m_cursorBlinkClock.restart();
-        }
+void InputField::onKeyPressed(const sf::Event::TextEvent& textEvent)
+{
+	if (currentlySelectedField == this)
+	{
+		if (textEvent.unicode == 8)
+		{
+			if (!m_content.empty())
+			{
+				m_content.pop_back();
+				m_text.setString(m_content);
+			}
+		}
+		else if (textEvent.unicode >= 32 && textEvent.unicode < 127)
+		{
+			m_content += static_cast<char>(textEvent.unicode);
+			m_text.setString(m_content);
+		}
+	}
+}
 
-        if (m_cursorVisible)
-        {
-            sf::FloatRect textBounds = m_text.getLocalBounds();
+void InputField::render()
+{
+	sf::RenderWindow& window = GameManager::getInstance()->getRenderWindow();
 
-            float cursorX = m_text.getPosition().x + textBounds.width + 1.f;
-            float cursorY = m_text.getPosition().y;
-            m_cursor.setPosition(cursorX, cursorY);
-            target.draw(m_cursor);
-        }
-    }
+	window.draw(m_box);
+	m_text.setString(m_content);
+	window.draw(m_text);
+
+	if (m_isSelected)
+	{
+		if (m_cursorBlinkClock.getElapsedTime().asSeconds() >= 0.5f)
+		{
+			m_cursorVisible = !m_cursorVisible;
+			m_cursorBlinkClock.restart();
+		}
+
+		if (m_cursorVisible)
+		{
+			sf::FloatRect textBounds = m_text.getLocalBounds();
+
+			float cursorX = m_text.getPosition().x + textBounds.width + 1.f;
+			float cursorY = m_text.getPosition().y;
+			m_cursor.setPosition(cursorX, cursorY);
+			window.draw(m_cursor);
+		}
+	}
 }
