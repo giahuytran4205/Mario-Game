94b42f257fffeec589d9b3fa521312f45ac5cf23
GiaHuy <giahuytran4205@gmail.com>
2024-12-13 16:55:07 +0700

    Update InputField

--- Mario Game/InputField.hpp
+++ Mario Game/InputField.hpp
@@ -1,22 +1,20 @@
 #pragma once
 
-#include "Enum.hpp"
-#include "Common.hpp"
-#include "SFML/Graphics.hpp"
-#include "TextureManager.hpp"
-#include "FontManager.hpp"
+#include "Rect.hpp"
 #include "GUI.hpp"
 #include "GameManager.hpp"
-
+#include <SFML/Graphics.hpp>
 #include <iostream>
-#include <fstream>
 #include <sstream>
+
+constexpr int INPUTFIELD_DEFAULT_RENDER_ORDER = 0;
 
 class InputField : public GUI
 {
 private:
-	void adjustTextPosCharSizeInBox(sf::Text& text, const sf::RectangleShape& box);
-	FRect getRectOfShape();
+	static InputField* currentlySelectedField;
+
+	void adjustTextPosCharSizeInBox();
 
 	sf::RectangleShape m_box;
 	sf::Text m_text;
@@ -27,19 +25,17 @@ private:
 	sf::Clock m_cursorBlinkClock;
 
 public:
-	static InputField* currentlySelectedField;
+	InputField(Object* parent = nullptr);
+	InputField(const sf::Vector2f& position, const sf::Vector2f& size, const sf::Font& font, Object* parent = nullptr);
 
-	InputField(Object* parent, sf::RenderWindow& window, const float& x, const float& y, const float& width, const float& height, sf::Font& font);
+	void configure(const sf::Vector2f& position, const sf::Vector2f& size, const sf::Font& font);
 
-	void onClick() override;
-	void onKeyPressed(const sf::Event::TextEvent& textEvent) override;
-
-	void setPosition(float x, float y);
-	void setSize(float width, float height);
-	sf::Vector2f getPosition();
-	sf::Vector2f getSize();
+	void setPosition(const sf::Vector2f& position);
+	void setSize(const sf::Vector2f& size);
 	std::string getContent();
 	void clearContent();
 
-	void draw(sf::RenderWindow& target);
+	void onClick() override;
+	void onKeyPressed(const sf::Event::TextEvent& textEvent) override;
+	void render() override;
 };
