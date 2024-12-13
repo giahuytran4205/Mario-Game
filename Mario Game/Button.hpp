71d6d9fb276f45eec90f50f90e7b3ae2ff5ad4df
GiaHuy <giahuytran4205@gmail.com>
2024-12-13 16:54:03 +0700

    Update Button

--- Mario Game/Button.hpp
+++ Mario Game/Button.hpp
@@ -1,38 +1,39 @@
 #pragma once
 
-#include "Common.hpp"
 #include "GUI.hpp"
 #include "TextView.hpp"
-#include "SFML/Graphics.hpp"
-#include "TextureManager.hpp"
 #include "GameManager.hpp"
-#include "Rect.hpp"
+#include <SFML/Graphics.hpp>
 
-constexpr unsigned int DEFAULT_TEXT_SIZE = 50;
+constexpr int BUTTON_RECTSHAPE_DEFAULT_RENDER_ORDER = 0;
+constexpr int BUTTON_TEXTVIEW_DEFAULT_RENDER_ORDER = 1;
 
 class Button : public GUI
 {
 private:
-	FRect getButtonRect();
-
-	sf::RectangleShape m_shape;
-	TextView m_label;
+	sf::RectangleShape m_rectShape;
+	TextView m_textView;
 	std::function<void()> m_action;
 
 public:
-	Button(Object* parent, sf::RenderWindow& window, float x, float y, float width, float height, const std::string& labelContent, sf::Font& labelFont, sf::Color labelColor, std::function<void()> action = nullptr);
+	Button(Object* parent = nullptr);
+	Button(sf::Vector2f position, sf::Vector2f size, const std::string& content, const sf::Font& font, std::function<void()> action = nullptr, Object* parent = nullptr);
 	~Button();
 
-	void setLabel(const std::string& s);
-	void setPosition(float x, float y);
-	void setSize(float width, float height);
-	void setContentScale(float x, float y);
+	void configure(sf::Vector2f position, sf::Vector2f size, const std::string& content, const sf::Font& font, std::function<void()> action = nullptr);
+
+	void setAction(std::function<void()> action);
+	void setPosition(const sf::Vector2f& position);
+	void setSize(const sf::Vector2f& size);
+	void setContent(const std::string& content);
+	void setFont(const sf::Font& font);
+	void setButtonFillColor(const sf::Color& color);
+	void setTextViewFillColor(const sf::Color& color);
+	void setTextViewRenderOrder(int renderOrder);
 
 	void onHovered() override;
 	void onUnhovered() override;
 	void onPressed() override;
 	void onClick() override;
-	void setAction(std::function<void()> action);
-
-	void draw(sf::RenderWindow& target);
+	void render() override;
 };
